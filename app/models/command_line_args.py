a9f325b72ac48d6555f9690ab857db56e065cebe
Kenneth Pouncey <kjpou@pt.lu>
2024-07-08 07:26:26 +0200

    circular dependency error.  removed constants references

--- app/models/command_line_args.py
+++ app/models/command_line_args.py
@@ -1,10 +1,8 @@
 from dataclasses import dataclass
 
-from app.helpers.constants import DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT
-
 
 @dataclass
 class CommandLineArgs:
-    server: str = DEFAULT_SERVER_HOST
-    port: int = DEFAULT_SERVER_PORT
+    server: str = "0.0.0.0"
+    port: int = 8036
     config: str = ""
